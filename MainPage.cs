352623a0418e6824c3c13db0c8e2558ceba1be7c
Mustafa Gönültaş <gonultasmf@hotmail.com>
2024-07-27 13:06:48 +0300

    finished.

--- MainPage.cs
+++ MainPage.cs
@@ -3,11 +3,37 @@ namespace FruitApp;
 
 public partial class MainPage : FmgLibContentPage
 {
-    public List<FruitInformation> PapayaInfo { get; set; }
+    public List<FruitInformation> PapayaInfo { get; set; } = new List<FruitInformation>
+              {
+                   new FruitInformation
+                   {
+                        Micronutrient = "Vitamin C",
+                        Percentage = 101
+                   },
+                   new FruitInformation
+                   {
+                        Micronutrient = "Vitamin A",
+                        Percentage = 19
+                   },
+                   new FruitInformation
+                   {
+                       Micronutrient = "Calcium",
+                       Percentage = 2
+                   },
+                   new FruitInformation
+                   {
+                       Micronutrient = "Magnesium",
+                       Percentage = 5
+                   },
+                   new FruitInformation
+                   {
+                       Micronutrient = "Potassium",
+                       Percentage = 5
+                   },
+                };
 
-    public MainPage()
+public MainPage()
     {
-        LoadData();
         this.BindingContext = this;
     }
 
@@ -83,37 +109,5 @@ public partial class MainPage : FmgLibContentPage
                 )
             )
         );
-    }
-
-    private void LoadData()
-    {
-        PapayaInfo = new List<FruitInformation>
-          {
-               new FruitInformation
-               {
-                    Micronutrient = "Vitamin C",
-                    Percentage = 101
-               },
-               new FruitInformation
-               {
-                    Micronutrient = "Vitamin A",
-                    Percentage = 19
-               },
-               new FruitInformation
-               {
-                    Micronutrient = "Calcium",
-                    Percentage = 2
-               },
-               new FruitInformation
-               {
-                    Micronutrient = "Magnesium",
-                    Percentage = 5
-               },
-               new FruitInformation
-               {
-                    Micronutrient = "Potassium",
-                    Percentage = 5
-               },
-          };
     }
 }
